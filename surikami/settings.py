4e30a51aed74f89b9ab2ed3ef1db29d1a0f30ec5
irfanzainudin <m.irfan.zain@gmail.com>
2025-05-20 19:28:40 +0800

    small improvements

--- surikami/settings.py
+++ surikami/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.localhost',
@@ -142,8 +143,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = ['static/']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+if DEBUG == True:
+    STATICFILES_DIRS = ['static/']
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
